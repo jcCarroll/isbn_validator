a6f7cb75ffd3fcecf488dd10fee597c34ab2ef44
jacob <2staffi@gmail.com>
2016-10-02 10:15:17 -0400

    returns only numbers and x

--- isbn_test.rb
+++ isbn_test.rb
@@ -11,4 +11,8 @@ class TestISBN <Minitest::Test
     def test_number_returns_only_numbers
         assert_equal("12345", valid("1!2@3#4$5%"))
     end
+
+    def test_number_returns_only_numbers_and_x
+        assert_equal("877195869x", valid("877195869x"))
+    end
 end
