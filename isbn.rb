ff5d24e0dcf2f260d8a9a34338ae0ce8a33aae42
jacob <2staffi@gmail.com>
2016-10-03 12:46:29 -0400

    toe up from the flo up... starting over again

--- isbn.rb
+++ isbn.rb
@@ -45,18 +45,4 @@ def valid(isbn)
     isbn.gsub!(/[^\dx]/,"")
     last = isbn[-1]
     short = isbn.chop
-    pos = 1
-    mult = ""
-    if short.length == 9
-        short.split("").each do |value|
-           mult << value.to_i * pos
-           pos = pos += 1
-        end
-        mult
-    elsif
-        short.length == 12
-        "valid"
-    else
-        "invalid"
-    end
 end
--- isbn_test.rb
+++ isbn_test.rb
@@ -4,39 +4,7 @@ require_relative "isbn.rb"
 
 class TestISBN <Minitest::Test
 
-    # def test_1_returns_1
-    #     assert_equal(1,1)
-    # end
-
-    # def test_number_returns_only_numbers
-    #     assert_equal("12345", valid("1!2@3#4$5%"))
-    # end
-
-    # def test_number_returns_only_numbers_and_x
-    #     assert_equal("032114653x", valid("0-321-14653-x"))
-    # end
-
-    # def test_number_returns_without_check_digit
-    #     assert_equal("032114653", valid("0-321-14653-0"))
-    # end
-
-    # def test_10_isbn_returns_valid
-    #     assert_equal("valid", valid("0-321-14653-0"))
-    # end
-
-    # def test_13_isbn_returns_valid
-    #     assert_equal("valid", valid("978-0-13-149505-0"))
-    # end
-
-    # def test_14_isbn_returns_invalid
-    #     assert_equal("invalid", valid("9768-0-13-149505-0"))
-    # end
-
-    # def test__isbn_returns_invalid
-    #     assert_equal("invalid", valid("9768-0-13-149505-0"))
-    # end
-
-    def test_mult_works
-        assert_equal(149162536496481, valid("1234567890"))
+    def test_1_returns_1
+        assert_equal(1,1)
     end
 end
