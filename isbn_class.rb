b600c956efba6bdf816b0521a30ca834bb419fca
jacob <2staffi@gmail.com>
2016-10-04 14:29:44 -0400

    removed ! set equal to isbn

--- isbn_class.rb
+++ isbn_class.rb
@@ -1,9 +1,9 @@
 def valid_isbn?(isbn)
-    remove_unwanted(isbn) # passes to remove_unwanted function   
+    isbn = remove_unwanted(isbn) # passes to remove_unwanted function   
     isbn.length == 10 # checks if length of isbn is 10 digits and returns true or false
 end
 
 def remove_unwanted(isbn)
-    isbn.delete!(" ") # delete spaces
-    isbn.delete!("-") # delete dashes
+    isbn = isbn.delete(" ") # delete spaces
+    isbn = isbn.delete("-") # delete dashes
 end
