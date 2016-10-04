10f9cb0db143fdbe0435219782d4af457fe62ebe
jacob <2staffi@gmail.com>
2016-10-04 13:59:06 -0400

    added comments

--- isbn_class.rb
+++ isbn_class.rb
@@ -1,4 +1,6 @@
 def valid_isbn?(isbn)
+    # removes spaces and replaces with nothing
     isbn.gsub!(" ", "")
+    # checks if length of isbn is 10 digits and returns true or false
     isbn.length == 10
 end
