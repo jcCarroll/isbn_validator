eaad6c4c01bbc96f6b64d9b5ba5315b215c4f996
jacob <2staffi@gmail.com>
2016-10-04 09:30:52 -0400

    from susan

--- isbn.rb
+++ isbn.rb
@@ -39,21 +39,89 @@
 # isbn = 7421394761
 
 
-def valid(isbn)
-    isbn.gsub!(/[^\dx]/,"")
-    last = isbn[-1]
-    short = isbn.chop
-    if isbn.length == 10
-        "valid"
-        multi10(isbn)
-    elsif
-        isbn.length == 13
-        "valid"
-    else
-        "invalid"
-    end
+def isbnlength(number)
+	valid10 = 0
+	valid13 = 0
+	if number.length == 10
+		valid10 = "true"
+	elsif number.length == 13
+	 	valid13 = "true"
+	end
 end
 
-def multi10(digits)
-    digits.to_i
+
+#function to check for valid ISBN 10 check digit
+def isbnvalidator10(number)
+	
+
+	#This removes the dash and space from the string
+	number = number.gsub(/[^0-9A-X]/i, "")
+
+	array10 = number.split(//,)
+	
+	if array10[9] == "X"
+		array10[9] = "10"
+	end
+	
+
+	#this portion pushes number into an array
+	array10 = array10.map(&:to_i)
+
+	
+	#this portion performs the math for the ISBN check digit
+	sum10 = 0
+
+	array10.each.with_index do |value, index|
+		break if index == 9
+		sum10 += value * (index +1)
+	end
+
+	checksum10 = sum10 % 11
+
+	if checksum10 == array10[9]
+		true
+	else
+		false
+	end
+
+	checksum10 == array10[9]
+
+end
+
+#function to check for valid ISBN 13 check digit
+def isbnvalidator13(number)
+	
+	#This removes the dash and space from the string
+	number = number.gsub(/[^0-9A-X]/i, "")
+
+	array = number.split(//,)
+		
+	#this portion pushes number into an array
+	array = array.map(&:to_i)
+	
+	#this portion performs the math for the ISBN check digit
+	sum = 0
+
+
+	array.each.with_index do |value, index|
+		break if index == 12
+		
+		if index % 2 == 0
+			sum += value * 1
+		else
+			sum += value * 3
+		end
+	end
+
+	checksum = sum % 10
+	checksum1 = (10 - checksum)
+
+	if checksum1 == array[12]
+		true
+	else
+		false
+	end
+
+	checksum == array[12]
+
 end
--- isbn_test.rb
+++ isbn_test.rb
@@ -1,22 +1,41 @@
 require "minitest/autorun"
 require_relative "isbn.rb"
 
+class TestISBN < Minitest::Test
 
-class TestISBN <Minitest::Test
+	def test_valid_10_digit_isbn_returns_valid
+		number = "0471958697"
+		assert_equal("true", isbnlength(number))
+	end
 
-    def test_1_returns_1
-        assert_equal(1,1)
-    end
+	def test_valid_13_digit_isbn_returns_valid
+		number = "0471958697245"
+		assert_equal("true", isbnlength(number))
+	end
 
-    def test_isbn10_valid
-        assert_equal("valid", valid("0-321-14653-0"))
-    end
+	def test_valid_10_digit_check_digit_returns_valid
+		number = "0-13609181-4"
+		assert_equal(true, isbnvalidator10(number))
+	end
 
-    def test_isbn13_valid
-        assert_equal("valid", valid("978-0-13-149505-0"))
-    end
+	def test_valid_10_digit_check_digit_returns_invalid
+		number = "817525766-7"
+		assert_equal(false, isbnvalidator10(number))
+	end
 
-    def test_non_isbn10or13_invalid
-        assert_equal("invalid", valid("978-0-1-149505-0"))
-    end
+	def test_valid_10_digit_check_digit_with_X_returns_valid
+		number = "0-76458572-X"
+		assert_equal(true, isbnvalidator10(number))
+	end
+
+	def test_valid_13_digit_check_digit_returns_valid
+		number = "978-3-16-148410-0"
+		assert_equal(true, isbnvalidator13(number))
+	end
+
+	def test_valid_13_digit_check_digit_returns_invalid
+		number = "345-3-16-148410-0"
+		assert_equal(false, isbnvalidator13(number))
+	end
+
 end
