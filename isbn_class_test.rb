e8deaf5152ab37f54e433af33f7d45a2a628857f
jacob <2staffi@gmail.com>
2016-10-05 08:52:33 -0400

    valid isbn thirteen with dashes returns true

--- isbn_class_test.rb
+++ isbn_class_test.rb
@@ -23,9 +23,13 @@ class TestISBN < Minitest::Test
                     assert_equal(true, valid_isbn?("9 780 4700 5902 9"))
                 end
 
-    def test_valid_isbn_with_dashes_returns_true
+    def test_valid_isbn_ten_with_dashes_returns_true
         assert_equal(true, valid_isbn?("0-471-95869-7"))
     end
+
+                def test_valid_isbn_thirteen_with_dashes_returns_true
+                    assert_equal(true, valid_isbn?("0-471-95869-7"))
+                end
 
     def test_valid_isbn_with_dashes_and_spaces_returns_true
         assert_equal(true, valid_isbn?("0-471-958 69 7"))
