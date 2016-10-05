018cec3f8386e52b24408dc3daa2a00b604887b8
jacob <2staffi@gmail.com>
2016-10-05 09:54:55 -0400

    not working 13

--- isbn_class.rb
+++ isbn_class.rb
@@ -49,8 +49,10 @@ end
                                 sum += value * 3 # sets sum equal to the sum plus the value times 3
                             end
                         pre_check_sum = sum % 10 # sets pre_check_sum equal to sum modulo 10
-                        check_sum = (10 - pre_check_sum) # sets check_sum equal to ten minus pre_check_sum
-                            if check_sum == thirteen_digit_array[12] # looks to see if check_sum is equal to position 12 of thirteen_digit_array and returns true or false
+                        check_sum = 10 - pre_check_sum # sets check_sum equal to ten minus pre_check_sum
+                        end_check_sum = pre_check_sum % 10
+                        end_check_sum_string = end_check_sum.to_i
+                            if end_check_sum_string == isbn[-1] # looks to see if check_sum is equal to position 12 of thirteen_digit_array and returns true or false
                                 true
                             else
                                 false
